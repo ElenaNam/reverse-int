7717dfcce924c4806332e1a29b922b2b05bfe4f9
ElenaNam <zaidelsonelena@gmail.com>
2020-10-20 13:44:56 +0400

    feat: add solution task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse (n) {    
+    let a = n.toString();  
+    let b = a.split('');  
+    let c = b.reverse();
+    
+    if ((c[c.length - 1]) === '-'){
+      c.pop();        
+      let d = c.join('');  
+      return +d;
+    } else if (c[0] === '0'){
+      c.shift();
+      return +c.join('');
+    } return +c.join(''); 
+    
+  }
